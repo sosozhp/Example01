dfbe50b244856693b5c3e34c410559bf94c1acb8
zhenghp <sosozhp@gmail.com>
2017-04-09 22:44:08 +0900

    pomにてlombokのdependency追加

--- mbg-tutorial/src/main/java/net/harawata/domain/Car.java
+++ mbg-tutorial/src/main/java/net/harawata/domain/Car.java
@@ -1,12 +1,13 @@
 package net.harawata.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by soso on 4/9/17.
  */
 
-@Data
 public class Car {
 
     private String name;
@@ -15,8 +16,27 @@ public class Car {
 
     private String maker;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getColor() {
+        return color;
+    }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
 
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
 }
---
+++ mbg-tutorial/src/main/java/net/harawata/domain/CarMainClass.java
@@ -0,0 +1,25 @@
+package net.harawata.domain;
+
+/**
+ * Created by soso on 4/9/17.
+ */
+public class CarMainClass {
+
+    public static void main(String args[]){
+
+        Car c = new Car();
+        c.setName("my car name");
+        c.setColor("red");
+        c.setMaker("toyota");
+
+        System.out.print(c.getColor());
+
+    }
+
+
+
+
+
+
+
+}
