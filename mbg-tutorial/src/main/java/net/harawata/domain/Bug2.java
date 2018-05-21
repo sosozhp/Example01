8fdb3c22532ac066f94374cbfe4252e4123f6885
zhenghp <sosozhp@gmail.com>
2018-05-22 00:01:53 +0900

    commit a big bug.

---
+++ mbg-tutorial/src/main/java/net/harawata/domain/Bug2.java
@@ -0,0 +1,17 @@
+package net.harawata.domain;
+
+import lombok.Data;
+
+/**
+ * Created by soso on 4/9/17.
+ */
+
+@Data
+public class Bug2 {
+
+    private String bugname;
+
+    private String auth;
+
+
+}
