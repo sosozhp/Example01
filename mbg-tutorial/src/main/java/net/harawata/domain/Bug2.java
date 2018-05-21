0c6583cd6be078248395026c8d7964f5a3f60a4b
zhenghp <sosozhp@gmail.com>
2018-05-22 00:15:50 +0900

    aa

---
+++ mbg-tutorial/src/main/java/net/harawata/domain/Bug2.java
@@ -0,0 +1,18 @@
+package net.harawata.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by soso on 4/9/17.
+ */
+
+public class Bug2 {
+
+    private String name;
+
+    private String bug2name;
+
+  
+}
